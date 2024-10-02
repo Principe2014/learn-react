7b82a5aae64bbaa3f403bd3950d228082a3b79f8
Principe2014 <112664140+Principe2014@users.noreply.github.com>
2024-10-02 18:48:39 -0400

    Created the MyComp component

--- src/pages/qcomps/firstcomp.tsx
+++ src/pages/qcomps/firstcomp.tsx
@@ -1,1 +1,15 @@
-function MyComp() {}
+import React from 'react';
+
+function MyComp() {
+
+    return (
+        <div>
+            <h1>My first component</h1>
+            <ol>
+                <li>Components: UI Building BLock</li>
+                <li>Defining a Component</li>
+                <li>Using a component</li>
+            </ol>
+        </div>
+    );
+}
